d92eeeb67ac5e98c5752e4f2e86f4f2c0527317f
xhagrg <grg.iksha@hotmail.com>
2023-07-15 18:39:56 -0500

    Use sm-docker instead of docker.

--- chapter-2/build_and_push.sh
+++ chapter-2/build_and_push.sh
@@ -1,33 +1,9 @@
 %%sh
+# make sure sm-docker is installed.
+pip install sagemaker-studio-image-build
 
-# The name of our algorithm
-algorithm_name=sagemaker-finetuning-endpoint
+# Change directory to chapter-2 of tutorial
+cd ~/2023-igarss-tutorial/chapter-2/
 
-cd container
-
-account=$(aws sts get-caller-identity --query Account --output text)
-
-# Get the region defined in the current configuration (default to us-west-2 if none defined)
-region=$(aws configure get region)
-region=${region:-us-west-2}
-
-fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
-
-# If the repository doesn't exist in ECR, create it.
-aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
-
-if [ $? -ne 0 ]
-then
-    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
-fi
-
-# Get the login command from ECR and execute it directly
-$(aws ecr get-login --region ${region} --no-include-email)
-
-# Build the docker image locally with the image name and then push it to ECR
-# with the full name.
-
-sm-docker build -q -t ${algorithm_name} .
-sm-docker tag ${algorithm_name} ${fullname}
-
-sm-docker push ${fullname}
+# Build the docker over code build and push to ECR
+sm-docker build .
